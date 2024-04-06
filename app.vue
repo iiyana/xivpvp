a0e0df041c123e5c38cdd9fbcb4c6941228fa322
iiyana <iyanacerus@gmail.com>
2024-04-06 14:18:38 -0400

    style and format fixes

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
-<script>
-
+<script setup>
+// const use0Opacity = ref(true)
+// this might break things idk hahahah
+// onNuxtReady(() => use0Opacity.value = false)
 </script>
 
 <!--
@@ -13,7 +15,7 @@
  -->
 <!-- https://stackoverflow.com/questions/68138453/how-create-a-true-sticky-header-footer-using-tailwindcss-sticks-to-bottom-even -->
 <template>
-    <div class="text-white flex flex-col min-h-screen">
+    <div class="text-white flex flex-col min-h-screen transition-opacity duration-500">
         <AppHeader />
         <div class="flex-grow flex flex-row justify-center md:items-center">
             <main>
@@ -23,3 +25,9 @@
         <AppFooter />
     </div>
 </template>
+
+<style>
+    .opacity-0 {
+        opacity: .1;
+    }
+</style>
--- components/Greeter.vue
+++ components/Greeter.vue
@@ -4,15 +4,14 @@ const pageList = ref([
     { num: '05', name: 'JOB GUIDES' },
     { num: '10', name: 'EVENTS' },
     { num: '20', name: 'CONTRIBUTE' },
-
 ])
 </script>
 
 <template>
-    <div class="grid md:grid-rows-2 md:grid-flow-col md:gap-4 place-content-center ">
+    <div class="grid md:grid-rows-2 md:grid-flow-col md:gap-x-10 place-content-center ">
         <!-- logo -->
-        <div class="order-1 md:row-span-1">
-            <div class="font-logo text-6xl tracking-tighter">
+        <div class="order-1 md:row-span-1 pb-6 md:pb-0">
+            <div class="font-logo text-6xl tracking-tighter pb-4">
                 <span>XIV</span>
                 <span class="text-primary">PVP</span>
             </div>
@@ -22,24 +21,34 @@ const pageList = ref([
         </div>
         <!-- learn more -->
         <div class="order-3 md:order-2 md:col-span-2 md:relative">
-            <div class="md:absolute md:inset-x-0 md:bottom-0">
+            <div class="md:absolute md:inset-x-0 md:bottom-0 text-sm">
                 <div>
                     Learn more
                 </div>
                 <div class="text-secondary">
-                    About Discord Github
+                    <NuxtLink to="/" class="pr-2 hover:text-white transition-all">
+                        About
+                    </NuxtLink>
+                    <NuxtLink to="/" class="pr-2 hover:text-white transition-all">
+                        Discord
+                    </NuxtLink>
+                    <NuxtLink to="/" class="pr-2 hover:text-white transition-all">
+                        Github
+                    </NuxtLink>
                 </div>
             </div>
         </div>
         <!-- pages -->
-        <div class="order-2 md:order-3 md:row-span-2">
-            <div v-for="(page, index) in pageList" :key="page.id" :class="{ 'pb-8': (index !== pageList.length - 1) }">
-                <span class="text-primary font-nums inline-block align-top">
-                    {{ page.num }}
-                </span>
-                <span class="font-outline-secondary tracking-tighter text-black text-4xl font-logo hover:font-outline-white transition-all">
-                    {{ page.name }}
-                </span>
+        <div class="order-2 pb-6 md:order-3 md:row-span-2 md:pb-0">
+            <div v-for="(page, index) in pageList" :key="page.id" :class="{ 'pb-9': (index !== pageList.length - 1) }">
+                <NuxtLink to="/">
+                    <span class="text-primary font-nums inline-block align-top tracking-tighter text-sm pr-1">
+                        {{ page.num }}
+                    </span>
+                    <span class="font-outline-secondary tracking-tighter text-black text-4xl font-logo hover:font-outline-white transition-all">
+                        {{ page.name }}
+                    </span>
+                </NuxtLink>
             </div>
         </div>
     </div>
